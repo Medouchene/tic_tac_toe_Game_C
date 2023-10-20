15391b9b132e0075d23224cd622b9e5125c58539
Medouchene <medoen02@gmail.com>
2023-10-20 16:54:58 +0200

    second commit

---
+++ TicTacGameFile1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+void resetBOard();
+void printBoard();
+int chackFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
+
+int main()
+{
+   
+   return 0;
+}
